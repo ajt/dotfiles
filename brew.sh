28141cafea9b38185beef5773f76407514796b5f
ajt <github@thorntonindustries.com>
2015-05-07 17:25:50 -0400

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -34,12 +34,9 @@ brew install wget --enable-iri
 # Install more recent versions of some OS X tools
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
-brew install homebrew/dupes/screen
-
 
 # Install other useful binaries
 brew install ack
-#install exiv2
 brew install git
 brew install imagemagick --with-webp
 brew install node # This installs `npm` too using the recommended installation method
@@ -48,7 +45,6 @@ brew install rename
 brew install tree
 brew install zopfli
 brew install ffmpeg --with-libvpx
-brew install android-platform-tools
 
 # Remove outdated versions from the cellar
 brew cleanup
